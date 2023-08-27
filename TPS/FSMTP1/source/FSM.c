5a096a9276b39f7e9692da95eb257527994f7dc3
LuIbanezz <luibanez@itba.edu.ar>
2023-08-27 18:57:35 -0300

    por favor que no rompa todo

---
+++ TPS/FSMTP1/source/FSM.c
@@ -0,0 +1,101 @@
+/***************************************************************************//**
+  @file     App.c
+  @brief    Application functions
+  @author   Bruno Di Sanzo
+ ******************************************************************************/
+
+/*******************************************************************************
+ * INCLUDE HEADER FILES
+ ******************************************************************************/
+
+#include "gpio.h"
+
+
+/*******************************************************************************
+ * CONSTANT AND MACRO DEFINITIONS USING #DEFINE
+ ******************************************************************************/
+
+
+/*******************************************************************************
+ * FUNCTION PROTOTYPES FOR PRIVATE FUNCTIONS WITH FILE LEVEL SCOPE
+ ******************************************************************************/
+
+
+/*******************************************************************************
+ *******************************************************************************
+                        GLOBAL FUNCTION DEFINITIONS
+ *******************************************************************************
+ ******************************************************************************/
+
+/**
+ * @brief
+ * @param
+ * @param
+ */
+void fsm (bool timer_sec, bool timer_min)
+{
+	switch(states)
+	{
+	case ID_ENTRY:
+
+		if(encoder) //aca habria que llamar a alguna funcion que devuelva que se esta ingresando la contra
+			states = ENCODER_ENTRY;
+		else if (card)
+			states = CARD_ENTRY;
+
+		break;
+
+	case ENCODER_ENTRY:
+			if(id_encoder == ENCODER)//hay que ver como se hace esto, y como es la base de datos
+				states = PIN_ENTRY;
+			else
+				states = RED_LED_ON;
+		break;
+
+	case CARD_ENTRY:
+			if(id_card == CARD)//hay que ver como se hace esto, y como es la base de datos
+				states = PIN_ENTRY;
+
+			else
+				states = RED_LED_ON;
+		break;
+
+	case PIN_ENTRY:
+			if()
+		break;
+
+	case RED_LED_ON:
+
+			break;
+
+	case GREEN_LED_ON:
+
+			break;
+
+	case BRIGHTNESS:
+
+			break;
+
+	case WRONG_ID:
+
+			break;
+
+	default:
+		break;
+	}
+
+}
+
+
+
+/*******************************************************************************
+ *******************************************************************************
+                        LOCAL FUNCTION DEFINITIONS
+ *******************************************************************************
+ ******************************************************************************/
+
+
+
+
+/*******************************************************************************
+ ******************************************************************************/
