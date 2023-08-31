3c050905b92aa3cb33429c813364853836927455
Juan Pablo <jcilfoneargibay@itba.edu.ar>
2023-08-31 14:53:30 -0300

    agregado funciones encoder

--- TPS/FSMTP1/Debug/source/App.d
+++ TPS/FSMTP1/Debug/source/App.d
@@ -1,48 +1,29 @@
 source/App.o source/App.d: ../source/App.c ../source/Drivers/board.h \
- ../source/Drivers/gpio.h ../source/Drivers/gpio.h ../SDK/CMSIS/MK64F12.h \
- ../SDK/CMSIS/core_cm4.h ../SDK/CMSIS/cmsis_version.h \
- ../SDK/CMSIS/cmsis_compiler.h ../SDK/CMSIS/cmsis_gcc.h \
- ../SDK/CMSIS/system_MK64F12.h ../SDK/startup/hardware.h \
+ ../source/Drivers/gpio.h ../SDK/CMSIS/MK64F12.h ../SDK/CMSIS/core_cm4.h \
+ ../SDK/CMSIS/cmsis_version.h ../SDK/CMSIS/cmsis_compiler.h \
+ ../SDK/CMSIS/cmsis_gcc.h ../SDK/CMSIS/system_MK64F12.h \
+ ../source/Drivers/gpio.h ../SDK/startup/hardware.h \
  ../SDK/CMSIS/fsl_device_registers.h ../SDK/CMSIS/MK64F12.h \
  ../SDK/CMSIS/MK64F12_features.h ../SDK/CMSIS/core_cm4.h \
  ../source/Drivers/Display.h ../source/Drivers/Encoder.h \
  ../source/EventQueue/queue.h ../source/FSM/FSM.h \
  ../source/FSM/../EventQueue/queue.h
-
 ../source/Drivers/board.h:
-
 ../source/Drivers/gpio.h:
-
-../source/Drivers/gpio.h:
-
 ../SDK/CMSIS/MK64F12.h:
-
 ../SDK/CMSIS/core_cm4.h:
-
 ../SDK/CMSIS/cmsis_version.h:
-
 ../SDK/CMSIS/cmsis_compiler.h:
-
 ../SDK/CMSIS/cmsis_gcc.h:
-
 ../SDK/CMSIS/system_MK64F12.h:
-
+../source/Drivers/gpio.h:
 ../SDK/startup/hardware.h:
-
 ../SDK/CMSIS/fsl_device_registers.h:
-
 ../SDK/CMSIS/MK64F12.h:
-
 ../SDK/CMSIS/MK64F12_features.h:
-
 ../SDK/CMSIS/core_cm4.h:
-
 ../source/Drivers/Display.h:
-
 ../source/Drivers/Encoder.h:
-
 ../source/EventQueue/queue.h:
-
 ../source/FSM/FSM.h:
-
 ../source/FSM/../EventQueue/queue.h:
--- TPS/FSMTP1/source/Drivers/Encoder.c
+++ TPS/FSMTP1/source/Drivers/Encoder.c
@@ -16,14 +16,14 @@
  * CONSTANT AND MACRO DEFINITIONS USING #DEFINE
  ******************************************************************************/
 
-
-
-
+#define IDLE 0
+#define ENCODER_CALLBACK_PERIOD 20000
+#define FIVE_SECOND_COUNTER 5 * S_TO_US / ENCODER_CALLBACK_PERIOD
 /*******************************************************************************
  * FUNCTION PROTOTYPES FOR PRIVATE FUNCTIONS WITH FILE LEVEL SCOPE
  ******************************************************************************/
-
-
+static int encoder=NO_MOVE;
+static int encoder_sw=RELEASED;
 /*******************************************************************************
  *******************************************************************************
                         GLOBAL FUNCTION DEFINITIONS
@@ -36,16 +36,17 @@ void Encoder_Init(void)
 	gpioMode(PIN_CH_A,INPUT);
 	gpioMode(PIN_CH_B,INPUT);
 	gpioMode(PIN_DEC_SW,INPUT);
-	//SysTick_Reg_Callback(EncoderStatus,20000);
+	SysTick_Reg_Callback(Encoder_Update,5000);
+	SysTick_Reg_Callback(EncoderSwitch_Update,20000);
 }
 
-int EncoderStatus(void)
+void Encoder_Update(void)
 {
 
 	static int state=IDLE;
 	bool CH_A=gpioRead(PIN_CH_A);
 	bool CH_B=gpioRead(PIN_CH_B);
-
+	encoder= IDLE;
 	/*****************************************/
 	// Máquina de Estados
 
@@ -80,7 +81,7 @@ int EncoderStatus(void)
 			if (CH_A & CH_B)
 			{
 				state=IDLE;
-				return ANTI_CLOCKWISE_TURN;
+				encoder= LEFT_TURN;
 			}
 			else if (!CH_A & !CH_B)
 				state=ACW2;
@@ -107,7 +108,7 @@ int EncoderStatus(void)
 			if (CH_A & CH_B)
 			{
 				state=IDLE;
-				return CLOCKWISE_TURN;
+				encoder= RIGHT_TURN;
 			}
 			else if (!CH_A & !CH_B)
 				state=CW2;
@@ -115,32 +116,54 @@ int EncoderStatus(void)
 				state=CW3;
 			break;
 	}
-	return IDLE;
+
 
 }
 
-int EncoderSwitchRead(void)
+void EncoderSwitch_Update(void)
 {
 	static int sw_state = LOW;
+	static int duration_counter=0;
 	bool sw_Read=gpioRead(PIN_DEC_SW);
 	if ((sw_state==LOW) && (sw_Read == LOW))
 	{
 		sw_state=HIGH;
-		return RISING_FLANK;
+		encoder_sw = RISING_FLANK;
+		duration_counter++;
 	}
 	else if (sw_state==LOW) // && sw_Read == HIGH pero no hace falta
 	{
-		return LOW;
+		encoder_sw = RELEASED;
+		duration_counter=0;
 	}
 	else if ((sw_state==HIGH) && (sw_Read == LOW))
 	{
-		return HIGH;
+		duration_counter++;
+		if (duration_counter==FIVE_SECOND_COUNTER)
+			encoder_sw=FIVE_SEC_PRESS;
+		else
+			encoder_sw=PRESSED;
 	}
 	else if (sw_state==HIGH)
 	{
 		sw_state=LOW;
-		return LOW;
+		encoder_sw = RELEASED;
+		duration_counter=0;
 	}
+}
+
+int getEncoderSwitch_State(void)
+{
+	int aux=encoder_sw;
+	encoder_sw=RELEASED;
+	return aux;
+}
+
+int getEncoder_State(void)
+{
+	int aux=encoder;
+	encoder=NO_MOVE;
+	return aux;
 }
 
 
