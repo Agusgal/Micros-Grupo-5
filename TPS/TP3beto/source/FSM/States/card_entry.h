0a0cb2055530ef4b81ce5a6d26dd72b32ae5f791
Agusgal <agaldeman@yahoo.com.ar>
2023-10-30 16:24:24 -0300

    beto is here

---
+++ TPS/TP3beto/source/FSM/States/card_entry.h
@@ -0,0 +1,15 @@
+#ifndef CARD_ENTRY_H
+#define CARD_ENTRY_H
+
+
+void init_cardswipe(void);
+
+void init_failed_cardswipe(void);
+
+void msg_fail_card(void);
+
+void msg_ok_card(void);
+
+void msg_error_card(void);
+
+#endif /* CARD_ENTRY_H */
