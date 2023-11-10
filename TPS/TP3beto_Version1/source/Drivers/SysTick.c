c33b7aa3a871b3a56c7bccf9a82f459a419538f5
Agusgal <agaldeman@yahoo.com.ar>
2023-11-10 10:58:22 -0300

    added Version1 to main

---
+++ TPS/TP3beto_Version1/source/Drivers/SysTick.c
@@ -0,0 +1,112 @@
+/***************************************************************************//**
+  @file     App.c
+  @brief    Application functions
+  @author   Bruno Di Sanzo
+ ******************************************************************************/
+
+/*******************************************************************************
+ * INCLUDE HEADER FILES
+ ******************************************************************************/
+
+#include "SysTick.h"
+#include "hardware.h"
+#include "gpio.h"
+#include "port.h"
+
+/*******************************************************************************
+ * CONSTANT AND MACRO DEFINITIONS USING #DEFINE
+ ******************************************************************************/
+
+#define TEST_PIN_2      10
+#define TEST_PORT_2     PC
+
+
+typedef struct SysTick_Callback_Element
+{
+	void (*fun_Callback)(void);
+	uint32_t num_Cycles;
+	uint32_t counter;
+}SysTick_Callback_Element_Type;
+
+
+// Array of stored callback functions
+static SysTick_Callback_Element_Type systick_Callback_Array [TOTAL_NUM_CALLBACK_FUNCTIONS];
+static uint32_t num_Callbacks = 0;
+
+
+/*******************************************************************************
+ * FUNCTION PROTOTYPES FOR PRIVATE FUNCTIONS WITH FILE LEVEL SCOPE
+ ******************************************************************************/
+__ISR__ SysTick_Handler(void);
+
+/*******************************************************************************
+ *******************************************************************************
+                        GLOBAL FUNCTION DEFINITIONS
+ *******************************************************************************
+ ******************************************************************************/
+
+/**
+ * @brief Initialize SysTic driver
+ * @return Initialization and registration succeed
+ */
+void SysTick_Init ()
+{
+	SysTick->CTRL = 0x00;
+	SysTick->LOAD = (__CORE_CLOCK__/ S_TO_US) * SYSTICK_ISR_PERIOD_US - 1; //12499999L; // <= 125 ms @ 100Mhz
+	SysTick->VAL  = 0x00;
+	SysTick->CTRL = SysTick_CTRL_CLKSOURCE_Msk | SysTick_CTRL_TICKINT_Msk | SysTick_CTRL_ENABLE_Msk;
+
+	gpioMode(PORTNUM2PIN(TEST_PORT_2, TEST_PIN_2), OUTPUT);
+}
+
+/**
+ * @brief Initialize SysTic driver
+ * @param funcallback Function to be call every SysTick
+ * @param period Period in which the function will be called in microseconds (us)
+ * @return Initialization and registration succeed
+ */
+bool SysTick_Reg_Callback (void (*funCallback)(void), uint32_t period)
+{
+	uint32_t num_Cycles = period / SYSTICK_ISR_PERIOD_US;
+
+	if(num_Callbacks > TOTAL_NUM_CALLBACK_FUNCTIONS - 1 || num_Cycles == 0)
+	{
+		return false;
+	}
+
+	SysTick_Callback_Element_Type element = {funCallback, num_Cycles, num_Cycles};
+	systick_Callback_Array[num_Callbacks++] = element;
+	return true;
+}
+
+
+/*******************************************************************************
+ *******************************************************************************
+                        LOCAL FUNCTION DEFINITIONS
+ *******************************************************************************
+ ******************************************************************************/
+__ISR__ SysTick_Handler(void)
+{
+
+	//test pin high
+	gpioWrite(PORTNUM2PIN(TEST_PORT_2, TEST_PIN_2), HIGH);
+
+	// Iterate through all the callbacks
+	for (uint32_t i = 0; i < num_Callbacks; i++)
+	{
+		systick_Callback_Array[i].counter--;
+		if (!systick_Callback_Array[i].counter) //If the counter reaches 0
+		{
+			(*systick_Callback_Array[i].fun_Callback)(); // Callback's calling.
+			systick_Callback_Array[i].counter = systick_Callback_Array[i].num_Cycles;	  //Counter re-establishment.
+		}
+	}
+
+	//Test pin low
+	gpioWrite(PORTNUM2PIN(TEST_PORT_2, TEST_PIN_2), LOW);
+}
+
+
+
+/*******************************************************************************
+ ******************************************************************************/
---
+++ TPS/TP3beto_Version1/source/User/Debug/source/User/userInput.d
@@ -0,0 +1,22 @@
+source/User/userInput.o source/User/userInput.d: \
+ ../source/User/userInput.c ../source/User/userInput.h \
+ ../source/EventQueue/queue.h ../SDK/CMSIS/MK64F12.h \
+ ../SDK/CMSIS/core_cm4.h ../SDK/CMSIS/cmsis_version.h \
+ ../SDK/CMSIS/cmsis_compiler.h ../SDK/CMSIS/cmsis_gcc.h \
+ ../SDK/CMSIS/system_MK64F12.h
+
+../source/User/userInput.h:
+
+../source/EventQueue/queue.h:
+
+../SDK/CMSIS/MK64F12.h:
+
+../SDK/CMSIS/core_cm4.h:
+
+../SDK/CMSIS/cmsis_version.h:
+
+../SDK/CMSIS/cmsis_compiler.h:
+
+../SDK/CMSIS/cmsis_gcc.h:
+
+../SDK/CMSIS/system_MK64F12.h:
