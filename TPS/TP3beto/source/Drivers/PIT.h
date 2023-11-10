f348e6356093e3e0142dfb2b89e5a062b3c266c3
Agusgal <agaldeman@yahoo.com.ar>
2023-11-10 10:53:52 -0300

    added pit to beto

---
+++ TPS/TP3beto/source/Drivers/PIT.h
@@ -0,0 +1,57 @@
+/*
+ * PIT.h
+ *
+ *  Created on: 5 nov. 2023
+ *      Author: agus
+ */
+
+
+
+#ifndef PIT_H_
+#define PIT_H_
+
+#include <stdint.h>
+#include <stdbool.h>
+
+/*******************************************************************************
+ * CONSTANT AND MACRO DEFINITIONS USING #DEFINE
+ ******************************************************************************/
+
+
+/**
+ * @brief Initializes PIT module
+ * @param
+ * @return
+ */
+void PIT_Init(void);
+
+
+/**
+ * @brief setea valor del timer de PIT.
+ * @param valor 	de tiempo entre intervalos de PIT. IMPORTANTE, se debe ingresar el tiempo dividido 20 ns.
+ * @param clb 		callback a ejecutarse junto al timer.
+ * @return
+ */
+void PIT_set_Timer(uint8_t id, uint16_t value, void(*clb)(void));
+
+
+/**
+ * @brief Comienza conteo de timer
+ * @param id que timer se quiere iniciar.
+ * @return
+ */
+void PIT_Start_Timer(uint8_t timer_id);
+
+/**
+ * @brief Para el temporizador.
+ * @param id     que timer se quiere parar.
+ * @return
+ */
+void PIT_Stop_Timer(uint8_t timer_id);
+
+
+
+#endif /* PIT_H_ */
+
+
+
